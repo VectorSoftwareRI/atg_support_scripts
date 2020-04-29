f8002a127e0e148da559bd4731bc7729ffab0a52
Artur Meski <artur.meski@vector.com>
2020-04-29 12:22:19 +0100

    Dependency checker, Manage project path check

--- check_deps.sh
+++ check_deps.sh
@@ -9,9 +9,14 @@ M_FAILED="${C_RED}failed${C_ZERO}"
 
 VC_VER=20
 
+failed=0
+
+MANAGE_PROJ_PATH="$1"
+
 echo -n "Checking VECTORCAST_DIR env variable... "
 if [[ -z "$VECTORCAST_DIR" ]];then
     echo -e "$M_FAILED"
+    failed=1
 else
     echo -e "$M_OK"
 fi
@@ -21,6 +26,7 @@ CLICAST_RES=$($VECTORCAST_DIR/clicast --version 2>&1)
 clicast_test=$?
 if [[ $clicast_test -ne 0 ]];then
     echo -e "$M_FAILED"
+    failed=1
 else
     echo -e "$M_OK"
 fi
@@ -31,6 +37,7 @@ if [[ $CLICAST_VER -ge $VC_VER ]];then
     echo -e "$M_OK"
 else
     echo -e "$M_FAILED"
+    failed=1
 fi
 
 echo -n "Checking Python3... "
@@ -38,6 +45,7 @@ PYTHON_VER=$(python3 --version 2>&1)
 python3_test=$?
 if [[ $python3_test -ne 0 ]];then
     echo -e "$M_FAILED"
+    failed=1
 else
     echo -e "$M_OK, $PYTHON_VER"
 fi
@@ -47,8 +55,62 @@ GIT_VER=$(git --version 2>&1)
 git_test=$?
 if [[ $git_test -ne 0 ]];then
     echo -e "$M_FAILED"
+    failed=1
 else
     echo -e "$M_OK, $GIT_VER"
 fi
+
+
+function check_manage
+{
+    MANAGE_PROJ_PATH="$1"
+
+    ext="${MANAGE_PROJ_PATH##*.}"
+
+    echo -n "Checking Manage project... "
+
+    if [[ "$ext" != "vcm" ]];then
+        echo -e "$M_FAILED, Invalid Manage project path"
+        failed=1
+        return
+    fi
+
+    if [[ -e "$MANAGE_PROJ_PATH" ]];then
+        echo -e "$M_OK"
+    else
+        echo -e "$M_FAILED, $MANAGE_PROJ_PATH is not a valid (existing) path"
+        failed=1
+        return
+    fi
+
+    comebackpath="$(pwd)"
+
+    cd $(dirname $MANAGE_PROJ_PATH)
+
+    echo -n "Checking if Manage project is under git control... "
+    git status >/dev/null 2>&1
+    git_status=$?
+    if [[ $git_status -eq 0 ]];then
+        echo -e "$M_OK"  
+    else
+        echo -e "$M_FAILED"
+    fi
+
+    cd $comebackpath
+
+}
+
+if [[ ! -z "$MANAGE_PROJ_PATH" ]];then
+    check_manage $MANAGE_PROJ_PATH
+fi
+
+if [[ $failed -eq 1 ]];then
+    RES="$M_FAILED"
+else
+    RES="$M_OK"
+fi
+echo 
+echo -e "RESULT: $RES"
+echo
 
 # EOF
