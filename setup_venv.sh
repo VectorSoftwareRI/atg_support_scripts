4f605932c4fce939a1920e430490465501161e22
Artur Meski <artur.meski@vector.com>
2020-05-01 11:11:48 +0100

    Clean-up

--- setup_venv.sh
+++ setup_venv.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 PYTHON_INTERPRETER=python3
-PIP_PATH=bin
 SCRIPT_PATH=$(realpath $0)
 SERVER_WORKSPACE=$(dirname $SCRIPT_PATH)
 cd $SERVER_WORKSPACE
@@ -13,8 +12,8 @@ if [[ -e "$VENV_DIR" ]];then
     exit 1
 fi
 
-${PYTHON_INTERPRETER} -m venv $VENV_DIR --without-pip
-source $VENV_DIR/${PIP_PATH}/activate
+${PYTHON_INTERPRETER} -m venv $VENV_DIR
+source $VENV_DIR/bin/activate
 curl https://bootstrap.pypa.io/get-pip.py | $PYTHON_INTERPRETER
 pip install -r $SERVER_WORKSPACE/requirements.txt
 
