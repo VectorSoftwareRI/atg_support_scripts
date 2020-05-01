7fbbe6de223bda34da46e1451c8827c95d70e599
Artur Meski <artur.meski@vector.com>
2020-05-01 11:16:19 +0100

    Removal of redundant code

--- setup_venv.sh
+++ setup_venv.sh
@@ -3,18 +3,15 @@
 PYTHON_INTERPRETER=python3
 SCRIPT_PATH=$(realpath $0)
 SERVER_WORKSPACE=$(dirname $SCRIPT_PATH)
-cd $SERVER_WORKSPACE
-
 VENV_DIR="$SERVER_WORKSPACE/venv"
 
 if [[ -e "$VENV_DIR" ]];then
-    echo "venv already exists, aborting"
+    echo "venv already exists... not creating"
     exit 1
 fi
 
 ${PYTHON_INTERPRETER} -m venv $VENV_DIR
 source $VENV_DIR/bin/activate
-curl https://bootstrap.pypa.io/get-pip.py | $PYTHON_INTERPRETER
 pip install -r $SERVER_WORKSPACE/requirements.txt
 
 #EOF
