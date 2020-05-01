bc6563da019b72cef8553b82ff06318e881d3b33
Andrew V. Jones <andrew.jones@vector.com>
2020-05-01 11:30:11 +0100

    Script clean-up

--- setup_venv.sh
+++ setup_venv.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 
+# The MIT License
+#
+# Copyright (c) 2020 Vector Informatik, GmbH. http://vector.com
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 PYTHON_INTERPRETER=python3
 SCRIPT_PATH=$(realpath $0)
 SERVER_WORKSPACE=$(dirname $SCRIPT_PATH)
@@ -10,9 +32,16 @@ if [[ -e "$VENV_DIR" ]];then
     exit 1
 fi
 
+# Create new venv
 ${PYTHON_INTERPRETER} -m venv $VENV_DIR
+
+# Activate it
 source $VENV_DIR/bin/activate
+
+# Update pip
 pip3 install -U pip
+
+# Install the required packages
 pip3 install -r $SERVER_WORKSPACE/requirements.txt
 
-#EOF
+# EOF
