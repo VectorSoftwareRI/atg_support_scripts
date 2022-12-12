833b4afd3f5ff7bb17d28f0e3cae9351bfeea300
Andrew V. Teylu <andrew.teylu@vector.com>
2022-12-12 11:03:01 +0000

    Processor for a single env

---
+++ bin/baseline_one_env.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+# Standard includes
+import pathlib
+import sys
+
+# Get our parent dir
+parent_dir = pathlib.Path(__file__).parent.parent.resolve()
+
+# Add it to the front of path
+sys.path.insert(0, str(parent_dir))
+
+# Grab the baseliner
+from atg_execution.baseline_for_atg import Baseline
+
+
+def baseline(env_script):
+    """
+    Baselines the given script 'env_script'
+    """
+    bl = Baseline(env_file=str(env_script), verbose=True, disable_failures=False)
+
+    # If you don't want to iterate at all
+    #
+    # FIXME: this is temporary and you don't really want this
+    max_iter = 1
+    bl.run(copy_out_manage=False, max_iter=max_iter)
+
+    return 0
+
+
+def main():
+    """
+    Main to help run a baseliner
+    """
+
+    # Correct number of args?
+    if len(sys.argv) != 2:
+        this_script = pathlib.Path(__file__).name
+        print("Incorrect arguments:")
+        print("")
+        print(f"    {this_script} <env name>")
+        print("")
+        return -1
+
+    # Correct type of first arg?
+    env_script = pathlib.Path(sys.argv[1])
+    if (not env_script.exists()) or env_script.suffix != ".env":
+        print(f"Provided env script {env_script} is not valid")
+        return -1
+
+    # Llets run!
+    return baseline(env_script)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
+
+# EOF
